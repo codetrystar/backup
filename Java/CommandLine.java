6e15455acec8d2080da7d95cd71c4a3b70950940
codetrystar <2032337948@qq.com>
2019-11-03 20:02:23 +0800

    add

---
+++ Java/CommandLine.java
@@ -0,0 +1,9 @@
+//javac CommandLine.java 
+//java CommandLine this is a command line 200 -100
+public class CommandLine {
+   public static void main(String args[]){ 
+      for(int i=0; i<args.length; i++){
+         System.out.println("args[" + i + "]: " + args[i]);
+      }
+   }
+}
